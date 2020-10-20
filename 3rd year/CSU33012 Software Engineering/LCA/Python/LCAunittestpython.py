b175d2dcb78b4d850fed5bea39381a2149b531ac
marycoyne <72067837+marycoyne@users.noreply.github.com>
2020-10-20 17:30:25 +0100

    Add files via upload

---
+++ 3rd year/CSU33012 Software Engineering/LCA/Python/LCAunittestpython.py
@@ -0,0 +1,32 @@
+import unittest
+import Node
+
+class LCAunittestpython(unittest.TestCase):
+
+    def test_node4_5(self):
+        self.assertEqual(Node.findLCA(Node.root, 4, 5).key, 2)
+
+    def test_node6_2(self):
+        self.assertEqual(Node.findLCA(Node.root, 6, 2).key, 1)
+
+    def test_node_none(self):
+        self.assertEqual(Node.findLCA(None, 4, 5), None)
+
+    def test_node1_5(self):
+        self.assertEqual(Node.findLCA(Node.root, 1, 5).key, 1)
+
+    def test_node_same(self):
+        self.assertEqual(Node.findLCA(Node.root, 1, 1).key, 1)
+
+    def test_node2_3(self):
+        self.assertEqual(Node.findLCA(Node.root, 2, 3).key, 1)
+
+    def test_node6_7(self):
+        self.assertEqual(Node.findLCA(Node.root, 6, 7).key, 3)
+
+    def test_node_fake(self):
+        self.assertEqual(Node.findLCA(Node.root, 8, 8), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
